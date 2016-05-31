5fd022fbab396a47ee4dae409d2b04d7a4e91aae
Owen Trueblood <hi@owentrueblood.me>
2016-05-30 20:05:42 -0400

    Sawtooth PWM.

--- analog-sticker.asm
+++ analog-sticker.asm
@@ -3,19 +3,57 @@
 .cseg
 .org 0
     rjmp    reset
+    nop     ; int0
+    nop     ; pcint0
+    nop     ; timer1 compare
+    nop     ; timer1 overflow
+    nop     ; timer0 overflow
+    nop     ; eeprom ready
+    nop     ; analog comparator
+    nop     ; conversion_done
+
+delay:
+    clr     r0
+delay_loop:
+    dec     r0
+    brne    delay_loop
+    dec     r16
+    brne    delay
+    ret
 
 reset:
     ; setup the stack
     ldi     r16, low(RAMEND)
     out     SPL, r16
 
-    ; setup pins for io
-    sbi     DDRB, PB0
+    ; setup pins for IO
+    ldi     r16, 0xff
+    out     DDRB, r16
+
+    ; setup PWM
+
+    ; non-inverting fast PWM mode
+    ldi     r16, (1 << COM0A1) | (1 << COM0A0) | (3 << WGM00)
+    out     TCCR0A, r16
+
+    ; clock pwm with system clock (no prescaling)
+    ldi     r16, (0 << WGM02) | (1 << CS00)
+    out     TCCR0B, r16
+
+    ; set frequency
+    ldi     r16, 127
+    out     OCR0A, r16
 
 blink_loop:
-    sbi     PORTB, PB0
-    nop
-    cbi     PORTB, PB0
-    nop
+    dec     r17
+    out     OCR0A, r17
+
+    ldi     r16, 250
+    rcall   delay
+
+    cpi     r17, 0
+    brne    blink_loop
+
+    sbi     PINB, PB1
 
     rjmp    blink_loop
