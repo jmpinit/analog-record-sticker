c672cca2aae002a78c42aae3e1053a21623f1702
Owen Trueblood <hi@owentrueblood.me>
2016-06-14 00:46:57 -0400

    Interrupt 512 times in 60 seconds.

--- analog-sticker.asm
+++ analog-sticker.asm
@@ -1,11 +1,25 @@
 .include "tn85def.inc"
 
+.define PIN_IN          PB2
+.define PIN_OUT         PB0
+.define PIN_RECORD      PB1
+.define PIN_TRIGGER     PB4
+.define PIN_DONE        PB3
+
+.def scrap           = r19
+.def irq_scrap       = r20
+.def recording       = r21
+.def buffer_ptr_l    = r22
+.def buffer_ptr_h    = r23
+
+    ;out     OCR0A, r20
+
 .cseg
 .org 0
     rjmp    reset
     nop     ; int0
     nop     ; pcint0
-    nop     ; timer1 compare
+    rjmp    timer_tick
     nop     ; timer1 overflow
     nop     ; timer0 overflow
     nop     ; eeprom ready
@@ -13,8 +27,14 @@
     rjmp    conversion_done
 
 conversion_done:
-    in      r20, ADCH
-    out     OCR0A, r20
+    in      irq_scrap, ADCH
+    ; if recording
+    ;   save to current position in buffer
+    ;   increment position
+    reti
+
+timer_tick:
+    sbi     PINB, PIN_DONE
     reti
 
 delay:
@@ -32,8 +52,9 @@ reset:
     out     SPL, r16
 
     ; setup pins for IO
-    sbi     DDRB, PB0   ; PWM
-    cbi     DDRB, PB2   ; A1
+    sbi     DDRB, PIN_OUT   ; PWM
+    cbi     DDRB, PIN_IN    ; A1
+    sbi     DDRB, PIN_DONE
 
     ; setup ADC
 
@@ -63,8 +84,44 @@ reset:
     ldi     r16, 0
     out     OCR0A, r16
 
+    ; setup sample timer
+
+    ; clear on match & prescaler set to divide by 4096
+    ldi     r16, (1 << CTC1) | (1 << CS13) | (1 << CS12) | (0 << CS11) | (1 << CS10)
+    out     TCCR1, r16
+
+    ; interrupt on compare match
+    ldi     r16, (1 << OCIE1A)
+    out     TIMSK, r16
+
+    ; interrupt 512 times in 60 seconds (with prescaler at 4096 and 8 MHz system clk)
+    ; calculated by 8 MHz / 4096 / (512 bytes / 60 seconds)
+    ldi     r16, 229
+    out     OCR1C, r16
+
+    ; setup application state
+
+    clr     buffer_ptr_l
+    clr     buffer_ptr_h
+
     ; enable interrupts
     sei
 
-blink_loop:
-    rjmp    blink_loop
+    rjmp    start
+
+start_playback:
+    rjmp    wait_loop
+
+start_recording:
+    rjmp    wait_loop
+
+start:
+    rjmp    start
+wait_loop:
+    sbic    PINB, PIN_RECORD
+    rjmp    start_recording
+
+    sbic    PINB, PIN_TRIGGER
+    rjmp    start_playback
+
+    rjmp    wait_loop
